84bf6aa4486ec55588ecc9304f39af3194828763
bari1192 <barabas.m4rk@gmail.com>
2025-03-17 17:34:33 +0000

    TDD tesztek környezetének előkészítése.

--- backend/tests/Feature/PersonDatabaseTest.php
+++ backend/tests/Feature/PersonDatabaseTest.php
@@ -3,6 +3,7 @@
 namespace Tests\Feature;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Facades\Schema;
 use Tests\TestCase;
 
 class PersonDatabaseTest extends TestCase
@@ -12,5 +13,23 @@ class PersonDatabaseTest extends TestCase
     public function test_IsEmptyPersonDatabase(): void
     {
         $this->assertDatabaseEmpty('Persons');
+    }
+    use RefreshDatabase;
+
+    public function test_IsEmptyPersonsDatabaseTable(): void
+    {
+        $this->assertDatabaseEmpty('Persons');
+    }
+
+    public function test_IsPersonsTableHasValidKeys(){
+        $this->assertTrue(Schema::hasColumns('Persons', [
+            'firstname',
+            'lastname',
+            'email',
+            'phone',
+            'password',
+            'created_at',
+            'updated_at',
+        ]));
     }
 }
