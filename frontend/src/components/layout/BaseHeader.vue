ee905658fd4c9a33cf57e311880eebd266cfe7c8
bari1192 <barabas.m4rk@gmail.com>
2025-04-10 14:04:06 +0000

    Header keresési felület kezdetleges kialakítása és további képek implementálása

--- frontend/src/components/layout/BaseHeader.vue
+++ frontend/src/components/layout/BaseHeader.vue
@@ -5,6 +5,27 @@
         <span class="self-center text-2xl font-semibold">Buzz<i class="fa-solid fa-bolt text-sky-500"></i>Shop</span>
       </RouterLink>
 
+      <form class="w-1/2 xl:w-1/5">
+        <label for="default-search"
+          class="mb-2 text-sm font-medium sr-only text-white">Keresés</label>
+        <div class="relative">
+          <div class="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+            <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
+              fill="none" viewBox="0 0 20 20">
+              <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
+            </svg>
+          </div>
+          <input type="search" id="default-search"
+            class="block w-full p-4 ps-10 text-base font-semibold text-sky-600 border-2 border-sky-400 rounded-lg bg-gray-100/60 focus:ring-sky-500 focus:border-sky-500"
+            placeholder="Fehérjék, vitaminok, kulacsok...." required />
+          <button type="submit"
+            class="text-white absolute end-2.5 bottom-2.5 bg-sky-500 border border-sky-700 hover:bg-sky-600 focus:ring-4 focus:outline-none focus:ring-sky-300 font-medium rounded-lg text-sm px-4 py-2 ">Keresés</button>
+        </div>
+      </form>
+
+
+
       <div class="flex justify-between items-center p-3">
         <!--Kosár-->
         <div class="flex items-center space-x-6">
@@ -58,7 +79,8 @@
               <BaseMobileNavBarDrop linkText="/proteins/Proteins" menuOption="Fehérjék" class="no-transform" />
               <BaseMobileNavBarDrop linkText="/vitamin" menuOption="Multivitaminok" class="no-transform" />
               <BaseMobileNavBarDrop linkText="/vitamin" menuOption="Vitaminok" class="no-transform" />
-              <BaseMobileNavBarDrop linkText="/controll-formulas" menuOption="Testsúly-kontroll formulák" class="no-transform" />
+              <BaseMobileNavBarDrop linkText="/controll-formulas" menuOption="Testsúly-kontroll formulák"
+                class="no-transform" />
             </ul>
           </li>
 
@@ -111,26 +133,27 @@ const mobileToggleBrands = () => {
 @import url('https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css');
 
 @media (min-width: 768px) {
-  .nav-primary > li {
+  .nav-primary>li {
     transition: all 0.3s ease-in-out;
     transform: scale(1.05);
     will-change: transform;
     position: relative;
   }
 
-  .nav-primary > li:hover {
+  .nav-primary>li:hover {
     transform: scale(1.1);
     color: white;
   }
 }
 
 @media (max-width: 767px) {
-  .nav-primary > li,
-  .nav-primary > li:hover,
+
+  .nav-primary>li,
+  .nav-primary>li:hover,
   .no-transform,
   .no-transform:hover,
-  .no-transform > *,
-  .no-transform > *:hover {
+  .no-transform>*,
+  .no-transform>*:hover {
     transform: none !important;
     transition: none !important;
   }
@@ -154,6 +177,7 @@ const mobileToggleBrands = () => {
   .dropdown-menu :deep(li) {
     transition: background-color 0.2s ease-in-out;
   }
+
   .dropdown-menu :deep(li:hover) {
     background-color: rgb(96 165 250 / 0.1);
   }
